fbffd4501cafd63eac0cfc64a459b5fe97762bd6
Uzair Ahmed <uzairahmed4705@gmail.com>
2024-07-24 00:28:47 +0500

    final testing

--- src/app/_components/whoIsInfo.tsx
+++ src/app/_components/whoIsInfo.tsx
@@ -5,15 +5,17 @@ interface RawWhoIsProps {
 }
 
 export default function RawWhoIsInfo({ res_whois } : RawWhoIsProps ) {
-    const data = res_whois;
 
+    if (!res_whois || res_whois?.length === 0) {
+      return <div className="border-b border-gray-300 px-4 py-2 text-center capitalize">No data available</div>;
+    }
     return (
         <div className="overflow-y-auto p-6">
           <div className="w-full border border-gray-400 rounded-md bg-gray-200 mb-4 p-4">
             <div>
               <pre className="p-4 rounded-lg ">
                 {/* Render data on the screen */}
-                {data.map((entry: any, index: any) => (
+                {res_whois?.map((entry: any, index: any) => (
                   <div key={index} className="text-black-100">
                     {/* Skip the first time when entry.type === 'comments' */}
                     {entry.type === 'comments' && index === 0 ? null : (
