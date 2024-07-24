58362f5a477dd05d3375cd68b0a2ee6a30079223
Uzair Ahmed <uzairahmed4705@gmail.com>
2024-07-24 19:53:10 +0500

    removed unneccassary items

--- next.config.js
+++ next.config.js
@@ -10,10 +10,4 @@ const nextConfig = {
       distDir: 'build',
 };
 
-module.exports = {
-  cacheHandler: require.resolve('./cache-handler.js'),
-  cacheMaxMemorySize: 0, // disable default in-memory caching
-}
-
-
 module.exports = nextConfig
