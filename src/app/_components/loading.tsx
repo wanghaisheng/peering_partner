7cc8286b56638937e978c8dc21fa3abaf420f65f
Uzair Ahmed <uzairahmed4705@gmail.com>
2024-07-24 19:37:51 +0500

    added loading style module

--- src/app/_components/loading.tsx
+++ src/app/_components/loading.tsx
@@ -1,23 +1,13 @@
 'use client';
 import React from 'react';
+import styles from './loading.module.css'
 
 const Loading = () => {
   return (
     <div className="hidden lg:flex items-center justify-center h-full w-full mx-auto bg-white">
-      <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-blue-500"></div>
+      <div className={`${styles.animateSpin} rounded-full h-32 w-32 border-t-2 border-b-2 border-blue-500`}></div>
       <style jsx>{`
-        .animate-spin {
-          animation: spin 1s linear infinite;
-        }
-
-        @keyframes spin {
-          0% {
-            transform: rotate(0deg);
-          }
-          100% {
-            transform: rotate(360deg);
-          }
-        }
+        .
       `}</style>
     </div>
   );
