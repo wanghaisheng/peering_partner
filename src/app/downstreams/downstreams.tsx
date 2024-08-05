f67123aed44dd9e5794c75f39f32bfff08e70755
Uzair Ahmed <uzairahmed4705@gmail.com>
2024-08-05 19:11:43 +0500

    increased delay

--- src/app/downstreams/downstreams.tsx
+++ src/app/downstreams/downstreams.tsx
@@ -13,14 +13,10 @@ export default async function DownstreamsDetails({asn_number}: DownstreamsDetail
     if(!asn_number) {
         return;
     }
-    console.log('downstreams');
     const res_asn = await Fetcher.getASNData(asn_number);
-    console.log('res_asn');
     const res_asn_peers = await Fetcher.getPeersData(asn_number);
-    console.log('res_asn_peers');
     const res_asn_prefixes = await Fetcher.getPrefixData(asn_number);
     const res_asn_downstreams = await Fetcher.getDownstreamData(asn_number);
-    console.log('res_asn_downstreams');
 
     return (
         <>
